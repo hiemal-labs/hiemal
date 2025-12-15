7fe9e1e15dade0730146e99e4076c1b3ac51992f
syzygial <67280346+syzygial@users.noreply.github.com>
2025-12-14 19:45:09 -0500

    Fix failing test
    Signed-off-by: syzygial <67280346+syzygial@users.noreply.github.com>

--- test/python/test_opsgen.py
+++ test/python/test_opsgen.py
@@ -14,9 +14,9 @@ class TestOpGen(unittest.TestCase):
     }
     test_op_wrapper = gen_ops._gen_wrapper("dsp", test_op)
     self.assertTrue(test_op_wrapper["kwargs_typedef"] == \
-      "typedef struct { int a; void* b; } test_op_kwargs_t;")
+      "typedef struct { int a; void* b; } test_op_dsp_kwargs_t;")
     self.assertTrue(test_op_wrapper["op_wrapper_decl"] == \
-      "int hm_test_op(void *src, void *dest, unsigned int n_bytes, int a, void* b);")
+      "int hm_dsp_test_op(void *src, void *dest, unsigned int n_bytes, int a, void* b);")
   
 if __name__ == "__main__":
   unittest.main()
