3adcdaebb4bef63a0b1686c66f0ad82c872f04e3
syzygial <67280346+syzygial@users.noreply.github.com>
2026-01-29 21:20:19 -0500

    Set minimum python version to 3.10
    Signed-off-by: syzygial <67280346+syzygial@users.noreply.github.com>

--- src/core/include/intern/python.h
+++ src/core/include/intern/python.h
@@ -3,6 +3,11 @@
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+
+#if PY_VERSION_HEX < 0x030A0000
+#error "Minimum python version is 3.10"
+#endif
+
 #include <stddef.h>
 
 int add_format_types(PyObject *m);
